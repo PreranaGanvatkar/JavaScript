4deeebaa8cdb15a0d4a848f5bb1c230697c1fa03
PreranaGanvatkar <preranaganvatkar@gmail.com>
2022-01-10 21:07:12 +0530

    Updated

---
+++ JavaScript/Variable.js
@@ -0,0 +1,6 @@
+let myLuckyNumber=37;
+myLuckyNumber;
+
+
+let octopusLimbs=8;
+octopusLimbs;
---
+++ JavaScript/iseven testing.js
@@ -0,0 +1,31 @@
+
+
+
+
+//ISEVEN TESTING
+
+function isEven(num){
+    
+   if(num%2==0){
+       console.log("even");
+   }
+   
+    
+}
+
+//IF ,ELSE IF AND ELSE TESTING
+
+function getColor(phrase){
+   
+   if(phrase==='stop'){
+       console.log("red");
+   }else if(phrase==='slow'){
+       console.log("yellow");
+   }else if (phrase==='go'){
+       console.log("green");
+   }else{
+       console.log("purple");
+   }
+   
+    
+}
