7a8d9d1e67db271a5072b6d8c877107db09f8025
PreranaGanvatkar <preranaganvatkar@gmail.com>
2022-05-26 20:50:54 +0530

    add Variables

---
+++ JavaScript/Variable.js
@@ -0,0 +1,10 @@
+var x=30;
+var y=20;
+var z=x+y;
+var a=x-y;
+var b=x*y;
+var c=x/y;
+console.log("Sum of "+x+" and "+y+" is: ",z);
+console.log("Sub of "+x+" and "+y+" is: ",a);
+console.log("Mul of "+x+" and "+y+" is: ",b);
+console.log("Div of "+x+" and "+y+" is: ",c);
